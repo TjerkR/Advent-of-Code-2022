eea886cd3e058f5f69e082cb94275ee64120fd7b
TjerkR <tjerk.reintsema@gmail.com>
2022-12-01 08:53:45 +0100

    Solution day 1.

---
+++ advent_of_code_2022/01/script_01.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 00:08:00 2022
+
+@author: tjerk
+"""
+
+# %% Import packages
+
+import numpy as np
+import pandas as pd
+import lr_data_lib
+
+
+# %% Import data
+
+# data = pd.read_csv("C:/Users/tjerk/Little Rocket/Projects/Advent-of-Code-2022/advent_of_code_2022/01/data.txt")
+# data = data.squeeze()
+with open("C:/Users/tjerk/Little Rocket/Projects/Advent-of-Code-2022/advent_of_code_2022/01/data.txt") as f:
+    lines = f.read().splitlines()
+
+
+# %% Script
+
+total_calories = []
+calories = []
+for line in lines:
+    # if not line:
+    #     sum_ = sum(calories)
+    #     calories = []
+    #     total_calories.append(sum)
+    #     print("YES")
+    try:
+        calories.append(int(line))
+        # print(line)
+    except ValueError:
+        sum_ = sum(calories)
+        calories = []
+        total_calories.append(sum_)       
+        
+max_calories = max(total_calories)
+print(max_calories)
+
+
+# %% Part 2
+calories_series = pd.Series(total_calories)
+calories_series = calories_series.sort_values(ascending=False)
+
+answer = sum(calories_series.tolist()[0:3])
--- advent_of_code_2022/functions.py
+++ advent_of_code_2022/functions.py
@@ -5,3 +5,12 @@ Created on Tue Nov 29 16:47:11 2022
 @author: tjerk
 """
 
+# %% Import packages
+
+import numpy as np
+import pandas as pd
+import lr_data_lib
+
+
+# %% Function definitions
+
